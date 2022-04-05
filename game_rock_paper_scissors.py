2a019777ccddb8c5e85b6fefffd7bafbd59fbacf
DariaDolia <daria.dolia1@gmail.com>
2022-04-05 14:41:51 +0200

    Code with game

---
+++ game_rock_paper_scissors.py
@@ -0,0 +1,56 @@
+import random
+
+options = {1: 'камень', 2: 'ножницы', 3: 'бумага'}
+
+
+def user_choice():
+    while True:
+        user_option = input('\nВведите один из вариантов: камень, ножницы, бумага: ').lower()
+        if user_option not in options.values():
+            print('Некоректное значение. Повторите попытку')
+        else:
+            return user_option
+
+
+def winer(computer, user):
+    if computer == 'камень' and user == 'ножницы':
+        print('\nПобедил компьютер, так как камень разбивает ножницы')
+    elif computer == 'ножницы' and user == 'бумага':
+        print('\nПобедил компьютер, так как ножницы режут бумагу')
+    elif computer == 'бумага' and user == 'камень':
+        print('\nПобедил компьютер, так как бумага заворачивает камень')
+    elif user == 'камень' and computer == 'ножницы':
+        print('\nВы победили, так как камень разбивает ножницы')
+    elif user == 'ножницы' and computer == 'бумага':
+        print('\nВы победили, так как ножницы режут бумагу')
+    elif user == 'бумага' and computer == 'камень':
+        print('\nВы победили, так как бумага заворачивает камень')
+
+
+def repeat_game():
+    while True:
+        print('\nВы хотите продолжить игру?')
+        again = input('Введите да или нет: ').lower()
+        # repeat_choice = ['да', 'нет']
+        if again not in ['да', 'нет']:
+            print('\nВы ввели некорректное значение.')
+            print('Повторите попытку.')
+        else:
+            return again
+
+
+def main():
+    again = 'да'
+    while again == 'да':
+        while True:
+            user = user_choice()
+            computer = options[random.randint(1, 3)]  # рандомный выбор компьютера
+            print(f'Выбор компьютера: {computer}')
+            if user != computer:
+                break
+            print('\nНичья. Играем повторный раунд')
+        winer(computer, user)
+        again = repeat_game()
+
+
+main()
