f95c261193097792dcb0d6208100c8b73fed4c8d
DariaDolia <daria.dolia1@gmail.com>
2022-04-24 16:13:26 +0200

    Fix comments and add 3rd solution

--- high_and_low_numbers.py
+++ high_and_low_numbers.py
@@ -1,18 +1,22 @@
 def high_and_low(numbers):
-    numbers = numbers.rsplit()
+    numbers = numbers.split()
     higher_value = int(numbers[0])
     lowest_value = int(numbers[0])
-    for i in range(1, len(numbers)):
-        if int(numbers[i]) > higher_value:
-            higher_value = int(numbers[i])
-        if int(numbers[i]) < lowest_value:
-            lowest_value = int(numbers[i])
+    for value in numbers[1:]:
+        if int(value) > higher_value:
+            higher_value = int(value)
+        if int(value) < lowest_value:
+            lowest_value = int(value)
     numbers = ' '.join([str(higher_value), str(lowest_value)])
     return numbers
 
 def high_and_low(numbers):
-    numbers_list = [int(num) for num in sorted(numbers.split())]
+    numbers_list = [int(num) for num in numbers.split()]
     return f'{max(numbers_list)} {min(numbers_list)}'
+
+def high_and_low(numbers):
+    integer_list = [int(el) for el in numbers.split()]
+    return f'{sorted(integer_list)[-1]} {sorted(integer_list)[0]}'
 
 
 print(high_and_low('8 3 -5 42 -1 0 0 -9 4 7 4 -4'))
