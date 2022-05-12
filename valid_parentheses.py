4b9eb1a5cc5ab0648cde80641b90adf5290ed245
DariaDolia <daria.dolia1@gmail.com>
2022-05-12 13:59:23 +0200

    add more two solutions and assert tests

--- valid_parentheses.py
+++ valid_parentheses.py
@@ -1,4 +1,3 @@
-
 def valid_parentheses(string):
     left_braces = string.count('(')
     right_braces = string.count(')')
@@ -17,3 +16,41 @@ def valid_parentheses(string):
 
 
 print(valid_parentheses('(())((())'))
+
+
+def valid_parentheses_2(string):
+    count = 0
+    for i in string:
+        if i == '(':
+            count += 1
+        elif i == ')':
+            count -= 1
+        if count < 0:
+            return False
+    if count == 0:
+        return True
+    return False
+
+
+print(valid_parentheses_2("()"))
+
+
+def valid_parentheses_3(string):
+    braces_string = ''.join([s for s in string if s in '()'])
+    while '()' in braces_string:
+        braces_string = braces_string.replace('()', '', 1)
+    return not braces_string
+
+
+print(valid_parentheses_3("()"))
+
+
+assert valid_parentheses_3(")(()))") is False
+assert valid_parentheses_3("(())((()())())") is True
+assert valid_parentheses_3("(") is False
+assert valid_parentheses_3(")test") is False
+assert valid_parentheses_3('') is True
+assert valid_parentheses_3("hi())(") is False
+assert valid_parentheses_3("hi(hi)()") is True
+
+
