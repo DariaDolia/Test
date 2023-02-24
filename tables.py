1f2dc62631c1f81eb9c2b44c52c133f16ad47cb1
Dasha Dolia <daria.dolia1@gmail.com>
2023-02-24 11:25:58 +0100

    did simplier

--- tables.py
+++ tables.py
@@ -54,16 +54,16 @@ def table_output(cursor, full_data):
         list_of_col_data = [len(col_names[i])] + [len(str(data[i])) for data in full_data]
         max_width.append(max(list_of_col_data) + 3)
 
-    print('|'.join([col_names[num].center(width) for num, width in enumerate(max_width)]), '|')
-    print("-" * (sum(max_width)+len(full_data[0])))
+    table_title = '|'.join([col_names[num].center(width) for num, width in enumerate(max_width)]) + '|'
+    print(table_title + '\n' + "-" * len(table_title))
 
     for data in full_data:
         list_of_data = []
-        for i, (d, width) in enumerate(zip(data, max_width)):
+        for d, width in zip(data, max_width):
             if isinstance(d, str | int):
-                list_of_data.append(f'{data[i]:{width}}')
+                list_of_data.append(f'{d:{width}}')
                 continue
-            list_of_data.append(f'{data[i]:{width},.2f}')
-        print('|'.join(list_of_data), '|')
+            list_of_data.append(f'{d:{width},.2f}')
+        print('|'.join(list_of_data) + '|')
 
 
