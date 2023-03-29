bc741abd09a0d8c1990943beae28423d72df5f68
Dasha Dolia <daria.dolia1@gmail.com>
2023-03-29 19:11:35 +0200

    create class Result instead of if-else logic in main file

--- class_api_url.py
+++ class_api_url.py
@@ -1,21 +1,29 @@
 import requests
+from requests.exceptions import RequestException
 
 
 class BaseApi:
 
     @staticmethod
-    def get_response(url, headers=None, params=None):
-        response = requests.get(url, headers=headers, params=params)
-        rj = response.json()
-        return rj
+    def method_get(url, headers=None, params=None):
+        try:
+            response = requests.get(url, headers=headers, params=params)
+            return response.json()
+        except RequestException as error:
+            return error        # or this variant: return f'Error occurred'
 
 
 class IpAddressApi(BaseApi):
 
     url = 'https://api.ipify.org?format=json'
 
-    def __str__(self):
-        return f'Your IP-address is: {self.get_response(self.url)["ip"]}'
+    def get_result(self):
+        ip_info = self.method_get(self.url)
+
+        if isinstance(ip_info, RequestException):
+            print(ip_info)
+            return
+        print(f'Your IP-address is: {ip_info["ip"]}')
 
 
 class CityCoordinatesApi(BaseApi):
@@ -23,31 +31,34 @@ class CityCoordinatesApi(BaseApi):
     url = 'https://api.api-ninjas.com/v1/city'
     headers = {'X-Api-Key': 'cXI98FHH1z5uWErFnlFiTPLpqIbMIPN1QuhElaUN'}
 
-    def get_city_info(self):
+    def get_result(self):
         your_city = input('Enter a city: ').title()
-        params = {'name': {your_city}}
-        res_json = self.get_response(self.url, headers=self.headers, params=params)
-        if res_json:
-            return self.get_city_coordinates(res_json)
-        elif not res_json:
+        params = {'name': your_city}
+        city_info = self.method_get(self.url, headers=self.headers, params=params)
+
+        if isinstance(city_info, RequestException):
+            return city_info
+        elif not city_info:
             return 'There in no such city'
-        else:
-            return 'error'
+        return self.get_city_coordinates(city_info)
 
     @staticmethod
-    def get_city_coordinates(response):
-        city_name = response[0]['name']
-        latitude = response[0]['latitude']
-        longitude = response[0]['longitude']
+    def get_city_coordinates(city_info):
+        city_name = city_info[0]['name']
+        latitude = city_info[0]['latitude']
+        longitude = city_info[0]['longitude']
         return city_name, latitude, longitude
 
 
 class CurrentWeatherApi(CityCoordinatesApi):
 
-    def get_current_weather(self):
-        city_info = self.get_city_info()
+    def get_result(self):
+        city_info = super().get_result()
 
-        if isinstance(city_info, str):
+        if isinstance(city_info, RequestException):
+            print(city_info)
+
+        elif isinstance(city_info, str):
             print(city_info)
 
         elif isinstance(city_info, tuple):
@@ -55,7 +66,12 @@ class CurrentWeatherApi(CityCoordinatesApi):
             url = 'https://api.open-meteo.com/v1/forecast'
             params = {'latitude': latitude, 'longitude': longitude, 'current_weather': True}
 
-            weather_json = self.get_response(url, params=params)
+            weather_json = self.method_get(url, params=params)
+            if isinstance(weather_json, RequestException):
+                print(weather_json)
+            else:
+                current_temp = weather_json['current_weather']["temperature"]
+                print(f'The current temperature in {city_name} is {current_temp}')
 
-            current_temp = weather_json['current_weather']["temperature"]
-            print(f'The current temperature in {city_name} is {current_temp}')
+
+
--- main.py
+++ main.py
@@ -1,22 +1,12 @@
-import class_api_url
-from options_menu import options
+import options_menu
 
 
 def main():
-    while True:
-        chosen_option = options()
-        if chosen_option == 1:
-            ip_address_url = class_api_url.IpAddressApi()
-            print(ip_address_url)
-
-        elif chosen_option == 2:
-            weather_in_the_city = class_api_url.CurrentWeatherApi()
-            weather_in_the_city.get_current_weather()
-
-        elif chosen_option == 3:
-            return
-        else:
-            print('Your choice out of scope\n')
+    chosen_option = 1
+    while chosen_option != 3:
+        chosen_option = options_menu.options()
+        res = options_menu.Result
+        res.show_result(chosen_option)
 
 
 if __name__ == '__main__':
